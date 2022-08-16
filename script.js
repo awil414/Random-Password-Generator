a5c95fe3c07bfa7c9c4095ef190f5af945829bb7
Amber Wilson <amberwilson@MacBook-Pro.local>
2022-08-16 09:20:18 -0500

    adding else if for user input options

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var confirmNum;
 var confirmSpec;
 var confirmUp;
 var confirmLow;
-var enter;
+var select;
 
 // Password variables 
 lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q","r", "s", "t", "u", "v", "w", "x", "y", "z"];
@@ -37,21 +37,56 @@ function generatePassword() {
         confirmSpec = confirm("Would you like to use Special characters?");
     };
 
-    // If user enters all no options //
+    // If user enters all no options 
     if (!confirmLow && !confirmUp && !confirmNum && !confirmSpec) {
-        choices = alert("Please choose atleast one criteria!");
+        userChoices = alert("Please choose at least one criteria!");
     }
-    // Else if user enters all options //
+    // Else if user enters all options 
     else if (confirmLow && confirmUp && confirmNum && confirmSpec) {
-        choices = lower.concat(upper, number, special);
+        userChoices = lower.concat(upper, number, special);
     }
-    // Else if user enters 3 options //
+    // Else if user enters 3 options 
     else if (confirmLow && confirmUp && confirmNum) {
-        choices = lower.concat(upper, number);
+        userChoices = lower.concat(upper, number);
     }
-    else if  (confirmLow && confirmUp && confirmSpec) {
-        choices = lower.concat(upper, special);
+    else if (confirmLow && confirmUp && confirmSpec) {
+        userChoices = lower.concat(upper, special);
     }
-    
-}
-
+    else if (confirmLow && confirmNum && confirmSpec) {
+        userChoices = lower.concat(number, special);
+    }
+    else if (confirmNum && confirmUp && confirmSpec) {
+        userChoices = number.concat(upper, special);
+    }
+    // Else if user enters 2 options
+    else if (confirmLow && confirmUp) {
+        userChoices = lower.concat(upper);
+    }
+    else if (confirmLow && confirmNum) {
+        userChoices = lower.concat(number);
+    }
+    else if (confirmLow && confirmSpec) {
+        userChoices = lower.concat(special);
+    }
+    else if (confirmUp && confirmNum) {
+        userChoices = upper.concat(number); 
+    }
+    else if (confirmUp && confirmSpec) {
+        userChoices = upper.concat(special);
+    }
+    else if (confirmNum && confirmSpec) {
+        userChoices = number.concat(special);
+    }
+    // Else if user enters 1 option
+    else if (confirmLow) {
+        userChoices = lower;
+    }
+    else if (confirmUp) {
+        userChoices = upper;
+    }
+    else if (confirmNum) {
+        userChoices = number;
+    }
+    else if (confirmSpec) {
+        userChoices = special;
+    }
